5a3079b45b184b8b10d75d790a1eecec102b121d
Zahra Ardalani <zahra.ardalani@gmail.com>
2020-08-25 23:26:01 +0000

    WASD Movement

--- input.js
+++ input.js
@@ -1,12 +1,28 @@
-const setupInput = function() {
+let connection;
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
+  
   let handleUserInput = function(){
     stdin.on('data', input => {
+      //console.log(input)
       if (input === '\u0003') {
         process.exit();
+      }
+      if(input === 'w'){
+        connection.write("Move: up")
+      }
+      if(input === 'a'){
+        connection.write("Move: left")
+      }
+      if(input === 'd'){
+        connection.write("Move: right")
+      }
+      if(input === 's'){
+        connection.write("Move: down")
       }
     });
   }
